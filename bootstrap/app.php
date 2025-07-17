ce0d5196b7d06c05af98097e83c0278cf76551f2
Paloma <karpaloma@hotmail.com>
2025-07-17 14:19:52 +0200

    FIX:  update import url

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\HandleInertiaRequests;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\HandleInertiaRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
