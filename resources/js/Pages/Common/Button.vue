9298303b382c5d4ef5cfbd150b09c163cca8a976
Paloma <karpaloma@hotmail.com>
2025-07-17 11:59:41 +0200

    style: restyle common button

--- resources/js/Pages/Common/Button.vue
+++ resources/js/Pages/Common/Button.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="px-6 py-3 bg-blue-700 hover:bg-blue-600 hover:shadow-md ring ring-blue-500 shadow-blue-500/80 rounded-md text-white cursor-pointer">
+  <button 
+    class="px-6 py-3 bg-blue-700 hover:bg-blue-600 hover:shadow-md ring ring-blue-500 shadow-blue-500/80 rounded-md text-white cursor-pointer"
+    type="type"  
+  >
     {{ label }}
   </button>
 </template>
@@ -8,6 +11,11 @@ const props = defineProps({
   label:{
     type: String,
     default: ''
+  },
+
+  type:{
+    type:String,
+    default:'button'
   }
 })
 </script>
